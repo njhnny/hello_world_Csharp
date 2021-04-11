cbc881d07473a6b3961cf2779d0f9b0581eab3a8
Nick Hennessy <njhnny@gmail.com>
2021-04-11 11:21:29 -0700

    add leap year looping exercise

--- GroceryList.cs
+++ GroceryList.cs
@@ -1,16 +1,16 @@
-using System;
+// using System;
 
-class GroceryList
-{
-  static void Main()
-  {
-    string[] myGroceryList = {"eggs", "milk", "bread", "bananas", "cereal", "rice", "yogurt"};
-    int[] groceryListPrices = { 3, 6, 4, 2, 4, 4};
+// class GroceryList
+// {
+//   static void Main()
+//   {
+//     string[] myGroceryList = {"eggs", "milk", "bread", "bananas", "cereal", "rice", "yogurt"};
+//     int[] groceryListPrices = { 3, 6, 4, 2, 4, 4};
 
-    Console.WriteLine("My grocery list:");
-    for (int index = 0; index < myGroceryList.Length; index++)
-    {
-      Console.WriteLine(myGroceryList[index]);
-    }
-  }
-}
+//     Console.WriteLine("My grocery list:");
+//     for (int index = 0; index < myGroceryList.Length; index++)
+//     {
+//       Console.WriteLine(myGroceryList[index]);
+//     }
+//   }
+// }
