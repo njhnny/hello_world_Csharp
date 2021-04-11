4209c154bf7426e181de85c84cfa18c20413c827
Nick Hennessy <njhnny@gmail.com>
2021-04-11 10:31:15 -0700

    doubleIt exercise

--- Hello.cs
+++ Hello.cs
@@ -4,6 +4,10 @@ class Program
 {
   static void Main()
   {
-    Console.WriteLine("Hello World");
+    Console.WriteLine("Give me a number and I will double it for you!");
+    string stringNumber = Console.ReadLine();
+    int yourNumber = int.Parse(stringNumber);
+    int yourDoubledNumber = yourNumber * 2;
+    Console.WriteLine("I doubled your number for you: " + yourDoubledNumber);
   }
 }
